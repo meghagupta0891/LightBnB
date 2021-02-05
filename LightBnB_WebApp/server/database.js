429500919bb521031720a7f8133a5bc8de35336e
Megha <meghagupta0891@gmail.com>
2021-02-05 03:27:02 +0000

    Added sql query to get reservations based on guest_id

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -59,7 +59,11 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT * FROM users where reservations = $1
+  LIMIT $2
+  `,[guest_id,limit])
+  .then(res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
